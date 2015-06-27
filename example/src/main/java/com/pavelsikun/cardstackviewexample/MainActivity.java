05e3d639bcf41603851fa9a7cc704436aed910c8
mrbimc <pavel.sikun@gmail.com>
2015-06-27 15:44:00 +0300

    demo apk

---
+++ example/src/main/java/com/pavelsikun/cardstackviewexample/MainActivity.java
@@ -0,0 +1,57 @@
+package com.pavelsikun.cardstackviewexample;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.pavelsikun.cardstackview.CardStackView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int stackSize;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CardStackView stack = (CardStackView)findViewById(R.id.stack);
+        Button add = (Button) findViewById(R.id.add);
+        Button remove = (Button) findViewById(R.id.remove);
+
+        final TextView counter = (TextView) findViewById(R.id.stackSizeLabel);
+        counter.setText("Stack size = " + stackSize);
+        stackSize = stack.getStackSize();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stackSize++;
+                if(stackSize > 5) {
+                    stackSize = 5;
+                    Snackbar.make(findViewById(android.R.id.content), "5 is maximal possible value!", Snackbar.LENGTH_SHORT).show();
+                }
+                stack.setStackSize(stackSize);
+                counter.setText("Stack size = " + stackSize);
+            }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stackSize--;
+                if (stackSize < 0) {
+                    stackSize = 0;
+                    Snackbar.make(findViewById(android.R.id.content), "0 is minimal possible value!", Snackbar.LENGTH_SHORT).show();
+                }
+                stack.setStackSize(stackSize);
+                counter.setText("Stack size = " + stackSize);
+            }
+        });
+    }
+}
